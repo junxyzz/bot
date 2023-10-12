f9e2f60f1dbab39d8e5f140acc84aa8e317ad64d
junxyzz <147757885+junxyzz@users.noreply.github.com>
2023-10-13 06:04:17 +0800

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import os, sys, mencoba, jun__ganteng__bgt
-sys.stdout.write('\x1b]2;PANDA-X 7.0 VERSION\x07')
-try:os.system('git pull')
-except:pass
-mencoba.jun__ganteng__bgt.cuyy__()
-###----------------------------------------------->
-#     *JANGGAN DI PREMIN YA BANGSAD:)
-###----------------------------------------------->
-#*Thanks To Jun Pakaya*
