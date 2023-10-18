1b3d2fe7cb1a00344aac05f50a7846470cc02e11
junxyzz <147757885+junxyzz@users.noreply.github.com>
2023-10-19 01:55:23 +0700

    Add files via upload

---
+++ m.py
@@ -0,0 +1,19 @@
+import os,sys,T
+sys.stdout.write('\x1b]2;CRACK SIMPLE\x07')
+#<CONTROL>
+try:
+    os.system('git pull')
+except:pass
+try:
+    os.mkdir('res-cp')
+except:pass
+try:
+    os.mkdir('res-ok')
+except:pass
+try:
+    os.mkdir('touch .prox.txt')
+except:pass
+T.memasuki()
+###----------------------------------------------->
+#     *THANKS TO JUN PAKAYA:)
+###----------------------------------------------->
